4d7627c60b28f24952cefc5a953503f51f31d653
Filipe Avanzzo <fastnexi@gmail.com>
2025-04-05 23:42:50 -0300

    finish `status endpoint`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows[0].sum);
-  return response.status(200).json({
-    message: "olá amigos",
+  const updatedAt = new Date().toISOString();
+
+  const version = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+
+  const usedDb = process.env.POSTGRES_DB;
+
+  const usedConnections = await database.query({
+    text: "SELECT  count(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [usedDb],
+  });
+
+  const dbVersion = version.rows[0].server_version;
+  const dbMaxConnections = maxConnections.rows[0].max_connections;
+  const dbOpenConnections = usedConnections.rows[0].count;
+
+  console.log(usedConnections);
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: +dbMaxConnections,
+        opened_connections: +dbOpenConnections,
+      },
+    },
   });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,19 @@
 test("get to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   const status = response.status;
-
   expect(status).toBe(200);
+
+  const {
+    updated_at,
+    dependencies: { database },
+  } = await response.json();
+
+  const parsedDate = new Date(updated_at).toISOString();
+  expect(updated_at).toEqual(parsedDate);
+
+  expect(database.version).toEqual("16.0");
+
+  expect(database.max_connections).toEqual(100);
+
+  expect(database.opened_connections).toEqual(1);
 });
