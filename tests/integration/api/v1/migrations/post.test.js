e31afc27066780e6f98f23531d3c72ab158cd3cb
Filipe Avanzzo <fastnexi@gmail.com>
2025-06-01 11:11:00 -0300

    fix test workflow

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,29 +1,29 @@
-import database from "infra/database";
+import database from "infra/database.js";
+import orchestrator from "tests/orchestrator.js";
 
-beforeAll(cleanDatabase);
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await database.query("drop schema public cascade; create schema public;");
+});
 
-async function cleanDatabase() {
-  await database.query("DROP schema public cascade; CREATE schema public;");
-}
-
-test("post to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+test("POST to /api/v1/migrations should return 200", async () => {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
-  const body = await response.json();
-  console.log(body);
+  expect(response1.status).toBe(201);
 
-  expect(response.status).toBe(201);
-  expect(Array.isArray(body)).toBe(true);
-  expect(body.length).toBeGreaterThan(0);
-  expect(body.some((migration) => "path" in migration)).toBe(true);
+  const response1Body = await response1.json();
+
+  expect(Array.isArray(response1Body)).toBe(true);
+  expect(response1Body.length).toBeGreaterThan(0);
 
   const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
-  const body2 = await response2.json();
-
   expect(response2.status).toBe(200);
-  expect(Array.isArray(body2)).toBe(true);
-  expect(body2.length).toBe(0);
+
+  const response2Body = await response2.json();
+
+  expect(Array.isArray(response2Body)).toBe(true);
+  expect(response2Body.length).toBe(0);
 });
