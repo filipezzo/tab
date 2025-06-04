15416d7ad7b9bf91b43a07404ecbf040f3e7e71e
Filipe Avanzzo <fastnexi@gmail.com>
2025-06-04 09:24:02 -0300

    refactor: test and package.json refactor

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,22 +4,20 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("get to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const status = response.status;
-  expect(status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const {
-    updated_at,
-    dependencies: { database },
-  } = await response.json();
+      const responseBody = await response.json();
 
-  const parsedDate = new Date(updated_at).toISOString();
-  expect(updated_at).toEqual(parsedDate);
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(database.version).toEqual("16.0");
-
-  expect(database.max_connections).toEqual(100);
-
-  expect(database.opened_connections).toEqual(1);
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
 });
