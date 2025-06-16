c787b51fb6fd4856f552fcbfd0daceeca7f1edde
Filipe Avanzzo <fastnexi@gmail.com>
2025-06-16 12:22:12 -0300

    feat: create migrator model

---
+++ models/migrator.js
@@ -0,0 +1,52 @@
+import database from "infra/database";
+import migrationRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+
+const defaultMigrationOptions = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function runMigration(options) {
+  const pendingMigrations = await migrationRunner(options);
+  return pendingMigrations;
+}
+
+async function listMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const pendingMigrations = await runMigration({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
+    return pendingMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await runMigration({
+      ...defaultMigrationOptions,
+      dbClient,
+      dryRun: false,
+    });
+
+    return migratedMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+export const migrator = {
+  listMigrations,
+  runPendingMigrations,
+};
